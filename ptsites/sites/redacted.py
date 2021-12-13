486652507e590a7e55e47c9fe62f46ccfbf5f0e8
meatballgithub <61103816+meatballgithub@users.noreply.github.com>
2021-12-13 18:20:29 +0800

    漏了一个没merge上

--- ptsites/sites/redacted.py
+++ ptsites/sites/redacted.py
@@ -8,35 +8,16 @@ from ..utils.net_utils import NetUtils
 
 
 class MainClass(Gazelle):
-<<<<<<< HEAD:ptsites/sites/greatposterwall.py
-    URL = 'https://greatposterwall.com/'
-    USER_CLASSES = {
-        'share_ratio': [1.2, 1.2],
-        'downloaded': [214748364800, 10995116277760],
-        'days': [14, 140]
-=======
     URL = 'https://redacted.ch/'
-    USER_CLASSES = {
-        'uploaded': [536870912000],
-        'share_ratio': [0.65],
-        'days': [56]
->>>>>>> upstream/master:ptsites/sites/redacted.py
-    }
+    USER_CLASSES = {'uploaded': [536870912000], 'share_ratio': [0.65], 'days': [56]}
 
     def build_workflow(self, entry, config):
         return [
-            Work(
-                url='/',
-                method='get',
-<<<<<<< HEAD:ptsites/sites/greatposterwall.py
-                succeed_regex='<a href="user.php\\?id=\\d+" class="username">.*?</a>',
-                fail_regex=None,
-=======
-                succeed_regex='<h1 class="hidden">Redacted</h1>',
->>>>>>> upstream/master:ptsites/sites/redacted.py
-                check_state=('final', SignState.SUCCEED),
-                is_base_content=True
-            )
+            Work(url='/',
+                 method='get',
+                 succeed_regex='<h1 class="hidden">Redacted</h1>',
+                 check_state=('final', SignState.SUCCEED),
+                 is_base_content=True)
         ]
 
     @classmethod
@@ -48,28 +29,27 @@ class MainClass(Gazelle):
 
     def build_selector(self):
         selector = super(MainClass, self).build_selector()
-        NetUtils.dict_merge(selector, {
-            'detail_sources': {
-                'default': {
-                    'elements': {'table': '#content > div > div.sidebar > div:nth-child(1) > ul'}
+        NetUtils.dict_merge(
+            selector, {
+                'detail_sources': {
+                    'default': {
+                        'elements': {
+                            'table': '#content > div > div.sidebar > div:nth-child(1) > ul'
+                        }
+                    },
+                    'extend': {
+                        'link': '/ajax.php?action=community_stats&userid={}'
+                    }
                 },
-                'extend': {
-                    'link': '/ajax.php?action=community_stats&userid={}'
+                'details': {
+                    'join_date': {
+                        'regex': 'Joined: (.*?ago)',
+                        'handle': self.handle_join_date
+                    },
+                    'points': None,
+                    'hr': None
                 }
-            },
-            'details': {
-                'join_date': {
-<<<<<<< HEAD:ptsites/sites/greatposterwall.py
-                    'regex': '加入时间:(.*?)前',
-=======
-                    'regex': 'Joined: (.*?ago)',
->>>>>>> upstream/master:ptsites/sites/redacted.py
-                    'handle': self.handle_join_date
-                },
-                'points': None,
-                'hr': None
-            }
-        })
+            })
         return selector
 
     def handle_join_date(self, value):
